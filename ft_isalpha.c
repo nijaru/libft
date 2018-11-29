2d542dd1f887526bb4bb09ef781a2f5d3a72a53b
Nicholas Russo <nrusso@e1z3r5p25.42.us.org>
2018-11-28 21:24:17 -0800

    Add isalpha
    Signed-off-by: Nicholas Russo <nrusso@e1z3r5p25.42.us.org>

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nrusso <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/28 20:21:14 by nrusso            #+#    #+#             */
-/*   Updated: 2018/11/28 21:24:02 by nrusso           ###   ########.fr       */
+/*   Created: 2018/11/28 21:20:47 by nrusso            #+#    #+#             */
+/*   Updated: 2018/11/28 21:23:11 by nrusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <libft.h>
 
-# include <string.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-int ft_isalpha(int c);
-int ft_isascii(int c);
-int ft_isdigit(int c);
-int isupper(int c);
-int islower(int c);
-
-#endif
+int	ft_isalpha(int c)
+{
+	return (isupper(c) || islower(c));
+}
--- ft_isascii.c
+++ ft_isascii.c
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nrusso <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/28 20:21:14 by nrusso            #+#    #+#             */
-/*   Updated: 2018/11/28 21:24:02 by nrusso           ###   ########.fr       */
+/*   Created: 2018/11/28 21:11:50 by nrusso            #+#    #+#             */
+/*   Updated: 2018/11/28 21:16:14 by nrusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <libft.h>
 
-# include <string.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-int ft_isalpha(int c);
-int ft_isascii(int c);
-int ft_isdigit(int c);
-int isupper(int c);
-int islower(int c);
-
-#endif
+int	ft_isascii(int c)
+{
+	return (0 <= c && c < 128);
+}
