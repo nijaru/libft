e965fa92558b1827f99474acd7b06ef17e7556a9
Nick Russo <nijaru0x@gmail.com>
2019-01-14 11:12:08 -0800

    ft_memcmp.c: Add fn
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

---
+++ ft_memcmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrusso <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/27 21:32:41 by nrusso            #+#    #+#             */
+/*   Updated: 2018/08/28 22:33:21 by nrusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+int memcmp(const void* s1, const void* s2,size_t n)
+{
+    unsigned char *p1;
+    unsigned char *p2;
+
+    p1 = 0;
+    p2 = 0;
+    *p1 = (unsigned char)s1;
+    *p2 = (unsigned char)s2;
+    while (n--)
+        if (*p1 != *p2)
+            return (*p1 - *p2);
+        else
+            p1++,p2++;
+    return (0);
+}
