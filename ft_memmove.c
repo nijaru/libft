aa80047abf5881e5145aa67d585cbc80c4de1457
Nick Russo <nijaru0x@gmail.com>
2019-01-14 11:18:19 -0800

    ft_memmove.c: Add fn
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

---
+++ ft_memmove.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrusso <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/27 21:32:41 by nrusso            #+#    #+#             */
+/*   Updated: 2018/08/28 22:33:21 by nrusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#   include <stddef.h>
+#   include "libft.h"
+
+void    *ft_memmove(void *dst, const void *src, size_t n)
+{
+    unsigned char tmp[n];
+    ft_memcpy(tmp,src,n);
+    ft_memcpy(dst,tmp,n);
+    return (dst);
+}
