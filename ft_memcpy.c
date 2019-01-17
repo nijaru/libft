b0f43887a416680dc55b763093bd32f500c7d73b
Nick Russo <nijaru0x@gmail.com>
2019-01-17 14:45:31 -0800

    memcpy: add fn
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -12,11 +12,11 @@
 
 #   include <libft.h>
 
-void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void    *ft_memcpy(void *dst, const void *src, size_t n)
 {
     char *dp;
     char *sp;
-    
+
     dp = 0;
     sp = 0;
     *dp = (char)dst;
