69d865b70900921966cb6d4b1d55a20db109ab76
Nick Russo <nijaru0x@gmail.com>
2019-01-21 11:07:42 -0800

    strsub: add fn
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

---
+++ ft_strsub.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrusso <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/27 13:01:14 by nrusso            #+#    #+#             */
+/*   Updated: 2018/08/27 20:17:27 by nrusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#   include "libft.h"
+
+char    *ft_strsub(char const *s, unsigned int start, size_t len)
+{
+    char	*str;
+	int	    i;
+
+	str = ft_strnew(len);
+	if (!str)
+		return (0);
+	s = s + start;
+	i = 0;
+	while (i++ < len && (str[i] = s[i]));
+	return (str);
+}
