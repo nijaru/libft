326aa61391fa5479e820ba778d4dbee5464376f4
Nick Russo <nijaru0x@gmail.com>
2019-01-29 15:49:59 -0800

    lstnew: add fn
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

---
+++ ft_lstnew.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrusso <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/27 13:01:14 by nrusso            #+#    #+#             */
+/*   Updated: 2018/08/27 20:17:27 by nrusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#   include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list *list;
+
+	list = (t_list*)malloc(sizeof(t_list));
+	if (!list)
+		return (0);
+    list->next = 0;
+    if (!content)
+    {
+        list->content = 0;
+        list->content_size = 0;
+        return (list);
+    }
+    list->content = malloc(content_size);
+    if (!list->content)
+    {
+        free(list);
+        list = 0;
+    }
+    ft_memcpy(list->content, content, content_size);
+    list->content_size = content_size;
+    return (list);
+}
