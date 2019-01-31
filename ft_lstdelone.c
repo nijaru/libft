e21928c5a1a7d23f6ad1b4262fc9bb970ba8728f
Nick Russo <nijaru0x@gmail.com>
2019-01-30 16:56:32 -0800

    lstdelone: add fn
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

---
+++ ft_lstdelone.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrusso <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/27 13:01:14 by nrusso            #+#    #+#             */
+/*   Updated: 2018/08/27 20:17:27 by nrusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#   include "libft.h"
+
+void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
+{
+	if (!alst || !*alst)
+		return ;
+	if (!del)
+		del((*alst)->content, (*alst)->content_size);
+	free(*alst);
+	*alst = 0;
+}
