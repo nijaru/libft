ac94de2eca153b08894884a581d0a160bcae4f0a
Nick Russo <nijaru0x@gmail.com>
2019-01-31 15:35:27 -0800

    lstnew: fix norm
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -10,15 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#   include "libft.h"
+#   include  "libft.h"
 
-t_list	*ft_lstnew(void const *content, size_t content_size)
+t_list  *ft_lstnew(void const *content, size_t content_size)
 {
-	t_list *list;
+    t_list  *list;
 
-	list = (t_list*)malloc(sizeof(t_list));
-	if (!list)
-		return (0);
+    list = (t_list*)malloc(sizeof(t_list));
+    if (!list)
+        return (0);
     list->next = 0;
     if (!content)
     {
