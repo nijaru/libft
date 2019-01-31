1c1ea849f36176bb2fccb6a961334582683672fd
Nick Russo <nijaru0x@gmail.com>
2019-01-30 16:56:37 -0800

    lstdel: add fn
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

---
+++ ft_lstdel.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrusso <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/27 13:01:14 by nrusso            #+#    #+#             */
+/*   Updated: 2018/08/27 20:17:27 by nrusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#   include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void*, size_t))
+{
+	t_list *tmp;
+	t_list *list;
+
+	if (!alst)
+		return ;
+	list = *alst;
+	while (list)
+	{
+		tmp = list->next;
+		ft_lstdelone(&list, del);
+		list = tmp;
+	}
+	*alst = NULL;
+}
