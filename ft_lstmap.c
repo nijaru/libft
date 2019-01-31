2d6c009bb8693ebefa5df15b76720ff237f30b77
Nick Russo <nijaru0x@gmail.com>
2019-01-31 15:31:30 -0800

    lstmap: fix norm
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,10 +12,10 @@
 
 #   include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *))
+t_list  *ft_lstmap(t_list *lst, t_list *(*f)(t_list *))
 {
-    t_list		*head;
-	t_list		*i;
+    t_list	*head;
+	t_list	*i;
 
 	if (!lst)
 		return (0);
