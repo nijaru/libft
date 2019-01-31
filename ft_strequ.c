fb9caabfbdcd85c142a3b7cf363efafc84c39b7d
Nick Russo <nijaru0x@gmail.com>
2019-01-31 15:11:25 -0800

    strequ: use libft fn
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- ft_strequ.c
+++ ft_strequ.c
@@ -10,12 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#   include "libft.h"
+
 int ft_strequ(char const *s1, char const *s2)
 {
-    while (*s1 || *s2)
-    {
-        if (*s1++ != *s2++)
-        return (0);
-    }
+    if (!ft_strcmp(s1, s2))
+        return 0;
     return (1);
 }
