457053f94cec7115eea49b33ccff85c4f6bf8e0e
Nick Russo <nijaru0x@gmail.com>
2019-02-06 17:10:10 -0800

    isascii: norm
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,13 +6,13 @@
 /*   By: nrusso <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 21:11:50 by nrusso            #+#    #+#             */
-/*   Updated: 2018/11/28 21:16:14 by nrusso           ###   ########.fr       */
+/*   Updated: 2019/02/06 17:10:01 by nrusso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#   include "libft.h"
+#	include "libft.h"
 
-int ft_isascii(int c)
+int	ft_isascii(int c)
 {
-    return (0 <= c && c < 128);
+	return (0 <= c && c < 128);
 }
