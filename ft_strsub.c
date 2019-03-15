957c60c6bb5ce9a3dc9deae5118f8b9a1355fe8a
Nick Russo <nijaru0x@gmail.com>
2019-03-15 16:58:11 -0700

    strsub: fix norm
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- ft_strsub.c
+++ ft_strsub.c
@@ -10,18 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#   include "libft.h"
+#include "libft.h"
 
-char    *ft_strsub(char const *s, unsigned int start, size_t len)
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
     char	*str;
-	int	    i;
+	int		i;
 
-	str = ft_strnew(len);
 	if (!str)
 		return (0);
-	s = s + start;
+	str = ft_strnew(len);
+	if (!str)
+		return 0;
+	s = s + start;	
 	i = 0;
-	while (i++ < len && (str[i] = s[i]));
+	while (i < len) 
+	{
+		str[i] = s[i];
+		i++;
+	}
 	return (str);
 }
