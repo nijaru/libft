feb0c1d54b0fe09af698f9129817614eb0916001
Nick Russo <nijaru0x@gmail.com>
2019-03-19 19:49:02 -0700

    strequ: fix
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- ft_strequ.c
+++ ft_strequ.c
@@ -14,7 +14,8 @@
 
 int	ft_strequ(char const *s1, char const *s2)
 {
-	if (!ft_strcmp(s1, s2))
-		return (0);
-	return (1);
+	if (s1 && s2)
+		if (!ft_strcmp(s1, s2))
+			return (1);
+	return (0);
 }
