cc1decbb6c4b21803cfe809343df4c8f06621c34
Nick Russo <nijaru0x@gmail.com>
2019-03-19 18:56:50 -0700

    whitespace: add chars
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- whitespace.c
+++ whitespace.c
@@ -14,7 +14,8 @@
 
 int	whitespace(char c)
 {
-	if (c == 32 || c == '\t' || c == '\n')
+	if (c == 32 || c == '\t' || c == '\n' ||
+            c == '\r' || c == '\v' || c == '\f')
 		return (1);
 	return (0);
 }
