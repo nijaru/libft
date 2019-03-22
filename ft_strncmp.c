58143db6f9336635dcdb249b20f7ad1078d98f57
Nick Russo <nijaru0x@gmail.com>
2019-03-21 17:48:34 -0700

    strncmp: fix condition
    Signed-off-by: Nick Russo <nijaru0x@gmail.com>

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,8 +14,10 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n--)
-		if (*s1++ != *s2++)
-			return (*(unsigned char*)(s1 - 1) - *(unsigned char*)(s2 - 1));
-	return (0);
+	while (n-- && *s1 && (*s1 == *s2))
+	{
+		++s1;
+		++s2;
+	}
+	return (*(unsigned char*)(s1) - *(unsigned char*)(s2));
 }
