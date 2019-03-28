7058996d78638be5b8684ac4e6df116e27d9a201
Nicholas Russo <nrusso@e1z2r7p16.42.us.org>
2019-03-28 14:12:37 -0700

    whitespace: fix indent
    Signed-off-by: Nicholas Russo <nrusso@e1z2r7p16.42.us.org>

--- whitespace.c
+++ whitespace.c
@@ -15,7 +15,7 @@
 int	whitespace(char c)
 {
 	if (c == 32 || c == '\t' || c == '\n' ||
-            c == '\r' || c == '\v' || c == '\f')
+			c == '\r' || c == '\v' || c == '\f')
 		return (1);
 	return (0);
 }
