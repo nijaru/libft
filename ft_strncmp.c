69ddde548fc20ea56ebf805f96a00f779145f076
Nicholas Russo <nrusso@e1z2r7p16.42.us.org>
2019-03-28 16:49:42 -0700

    norm: cast spacing
    Signed-off-by: Nicholas Russo <nrusso@e1z2r7p16.42.us.org>

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -21,8 +21,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	c2 = 0;
 	while (n > 0)
 	{
-		c1 = (unsigned char) *s1++;
-		c2 = (unsigned char) *s2++;
+		c1 = (unsigned char)*s1++;
+		c2 = (unsigned char)*s2++;
 		if (c1 == 0 || c1 != c2)
 			return (c1 - c2);
 		--n;
