36087aa44374e1a6dfb1a2c2a0b215531c5720cd
Nicholas Russo <nrusso@e1z2r7p16.42.us.org>
2019-03-28 16:16:55 -0700

    strncmp: fix macos
    Signed-off-by: Nicholas Russo <nrusso@e1z2r7p16.42.us.org>

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,10 +14,18 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n-- && *s1 && (*s1 == *s2))
+	unsigned char c1;
+	unsigned char c2;
+
+	c1 = 0;
+	c2 = 0;
+	while (n > 0)
 	{
-		++s1;
-		++s2;
+		c1 = (unsigned char) *s1++;
+		c2 = (unsigned char) *s2++;
+		if (c1 == 0 || c1 != c2)
+			return (c1 - c2);
+		--n;
 	}
-	return (*(unsigned char*)(s1) - *(unsigned char*)(s2));
+	return (c1 - c2);
 }
