e90a8b8b1929150a420882aa4050418084ae412f
Nicholas Russo <nrusso@e1z2r7p10.42.us.org>
2019-04-01 21:36:10 -0700

    strdup: refactor
    Signed-off-by: Nicholas Russo <nrusso@e1z2r7p10.42.us.org>

--- ft_strdup.c
+++ ft_strdup.c
@@ -14,13 +14,12 @@
 
 char	*ft_strdup(const char *s1)
 {
-	size_t len;
-	void *new;
+	size_t	len;
+	void	*new;
 
 	len = ft_strlen(s1) + 1;
 	new = malloc(len);
 	if (!new)
 		return (0);
-
 	return ((char*)ft_memcpy(new, s1, len));
 }
