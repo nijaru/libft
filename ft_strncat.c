f630d702f5968d425c95ce1079317b9c8b05cd14
Nicholas Russo <nrusso@e1z2r7p22.42.us.org>
2019-04-03 17:14:31 -0700

    strncat: fix
    Signed-off-by: Nicholas Russo <nrusso@e1z2r7p22.42.us.org>

--- ft_strncat.c
+++ ft_strncat.c
@@ -14,10 +14,11 @@
 
 char	*ft_strncat(char *dst, const char *src, size_t n)
 {
-	char *s;
-	size_t slen;
+	char	*s;
+	size_t	slen;
 
 	s = dst;
+	dst += ft_strlen(dst);
 	slen = ft_strnlen(src, n);
 	dst[slen] = 0;
 	ft_memcpy(dst, src, slen);
