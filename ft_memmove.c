97faaadb403c951303b61e24e4fc83ce4fe7881e
Nicholas Russo <nrusso@e1z2r7p10.42.us.org>
2019-04-04 19:33:32 -0700

    memmove: refactor
    Signed-off-by: Nicholas Russo <nrusso@e1z2r7p10.42.us.org>

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,9 +14,16 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	unsigned char tmp[n];
+	char		*dp;
+	const char	*sp;
+	size_t		i;
 
-	ft_memcpy(tmp, src, n);
-	ft_memcpy(dst, tmp, n);
+	i = -1;
+	dp = (char*)dst;
+	sp = (const char*)src;
+	while (sp < dp && n--)
+		dp[n] = sp[n];
+	while (sp > dp && ++i < n)
+		dp[i] = sp[i];
 	return (dst);
 }
